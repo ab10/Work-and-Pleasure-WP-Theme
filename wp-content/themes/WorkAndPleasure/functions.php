31e9eca7e5e563f00b777d2cae1d00afea5f9c96
ab10 <miki.abtan@gmail.com>
2015-04-18 18:36:16 +0300

    Hoping for the best
    Added plain HTML and CSS of story page

--- wp-content/themes/WorkAndPleasure/functions.php
+++ wp-content/themes/WorkAndPleasure/functions.php
@@ -6,7 +6,31 @@ function register_my_menus() {
     )
   );
 }
+
+function baw_theme_setup() {
+  add_image_size( 'my-thumbnail', 346, 238, true );
+  add_image_size( 'img-in-story', 645 );
+}
+
+
+
+function my_custom_sizes( $sizes ) {
+    return array_merge( $sizes, array(
+        'img-in-story' => __( 'Image In Story' ),
+    ) );
+}
+
+
+function remove_width_attribute( $html ) {
+   $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+   return $html;
+}
+
 add_action( 'init', 'register_my_menus' );
 add_theme_support( 'post-thumbnails' );
-add_image_size( 'my-thumbnail', 346, 238, true );
+add_action( 'after_setup_theme', 'baw_theme_setup' );
+add_filter( 'image_size_names_choose', 'my_custom_sizes' );
+add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
+
+
 ?>
